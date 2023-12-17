d41ff7d5d4efc3266b26f000581d1f33c988e651
Timet-X <152591795+Timet-X@users.noreply.github.com>
2023-12-18 01:40:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let ws = new WebSocket("wss://websocket-1.onrender.com/:443");
 
+let controllTD = document.querySelector('.controllTD') ;
+controllTD.addEventListener('input', (event) => {
+  ws.send(JSON.stringify({ 'slider1': controllTD.value / 100 }));
+}, false);
+
 ws.addEventListener('open', (event) => {
-    console.log('Socket connection open!');
+  console.log('Socket connection open!');
+  // alert('Successfully connected to socket server 🎉');
+  ws.send('pong');
 });
 
 ws.addEventListener('error', (error) => {
     console.error('Error in the connection', error);
-    alert('Error connecting to socket server', error);
+    alert('error connecting socket server', error);
 });
 
 ws.addEventListener('close', (event) => {
     console.log('Socket connection closed');
-    alert('Closing socket server');
+    alert('closing socket server');
 });
-
-// Send text input data
-document.querySelector('input[type="submit"]').addEventListener('click', (event) => {
-    event.preventDefault();
-    let textInput = document.querySelector('#textInput').value;
-    ws.send(JSON.stringify({ 'text': textInput }));
-}, false);
